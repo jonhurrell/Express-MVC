186943c35070412b58b6f0e8cd3bbacfba42accb
Jon Hurrell <jonhurrell.digitaldwp@gmail.com>
2016-01-08 08:17:50 +0000

    Format whitespace to tab indentation.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,10 +1,10 @@
 var express = require('express'),
-    router  = express.Router();
+	router  = express.Router();
 
 module.exports = function (app) {
-  app.use('/', router);
+	app.use('/', router);
 };
 
 router.get('/', function (req, res) {
-    res.render('../views/index');
+	res.render('../views/index');
 });
